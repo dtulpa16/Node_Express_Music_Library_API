859616d9c3ba78f4f5bb0b56735a39663515242a
Daniel Tulpa <dtulpa16@yahoo.com>
2021-12-17 10:18:13 -0600

    Added PUT And POST Request In Index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const repoContext = require('./repository/repository-wrapper')
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(3000,function(){
     console.log("Server started. Lestening to port 3000.")
@@ -15,4 +17,16 @@ app.get('/api/products/:id',(req,res)=>{
     const id = req.params.id;
     const product = repoContext.products.findProductById(id);
     return res.send(product)
+})
+app.post('/api/products', (req,res)=>{
+    const newProduct = req.body;
+    const addedProduct = repoContext.products.createProduct(newProduct);
+    return res.send(addedProduct)
+})
+
+app.put('/api/products/:id',(req,res)=>{
+    const id = req.params.id;
+    const productToUpdate = req.body;
+    const updatedProduct = repoContext.products.updateProduct(id,productToUpdate);
+    return res.send(updatedProduct)
 })
