1a53f18edaa0e4ff3f8c658bc8a0f4e13b50f049
Daniel Tulpa <dtulpa16@yahoo.com>
2021-12-17 11:23:15 -0600

    Added Middleware For Music Requests + Added Get Funtions For Music Querying

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const repoContext = require('./repository/repository-wrapper')
+const cors = require('cors');
+const {validateProduct} = require('./middleware/products-validation')
+const {validateSong} = require("./middleware/music-validation")
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -8,7 +12,7 @@ app.listen(3000,function(){
     console.log("Server started. Lestening to port 3000.")
 })
 
-app.get('/api/products', (req, res) => {
+app.get('/api/products',(req, res) => {
     const products = repoContext.products.findAllProducts()
     return res.send(products)
     });
@@ -18,13 +22,13 @@ app.get('/api/products/:id',(req,res)=>{
     const product = repoContext.products.findProductById(id);
     return res.send(product)
 })
-app.post('/api/products', (req,res)=>{
+app.post('/api/products', [validateProduct] , (req,res)=>{
     const newProduct = req.body;
     const addedProduct = repoContext.products.createProduct(newProduct);
     return res.send(addedProduct)
 })
 
-app.put('/api/products/:id',(req,res)=>{
+app.put('/api/products/:id', [validateProduct] ,(req,res)=>{
     const id = req.params.id;
     const productToUpdate = req.body;
     const updatedProduct = repoContext.products.updateProduct(id,productToUpdate);
@@ -35,4 +39,15 @@ app.delete('/api/products/:id', (req,res)=>{
     const id = req.params.id;
     const deletedProduct = repoContext.products.deleteProduct(id);
     return res.send(deletedProduct)
+})
+
+app.get('/api/music', (req, res)=>{
+    const music = repoContext.songs.findAllSongs()
+    return res.send(music)
+})
+
+app.get('/api/music/:id',(req,res)=>{
+    let id = req.params.id
+    let song = repoContext.songs.findSongById(id)
+    return res.send(song)
 })
