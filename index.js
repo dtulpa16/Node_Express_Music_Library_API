50f84d10219eda4c978e4c1f48bc8669cec65c1f
Daniel Tulpa <dtulpa16@yahoo.com>
2021-12-17 09:47:04 -0600

    Added "Get" Function To Fetch All Products Or A Single Products Based On Its ID

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const repoContext = require('./repository/repository-wrapper')
 const app = express()
 
 app.listen(3000,function(){
     console.log("Server started. Lestening to port 3000.")
+})
+
+app.get('/api/products', (req, res) => {
+    const products = repoContext.products.findAllProducts()
+    return res.send(products)
+    });
+
+app.get('/api/products/:id',(req,res)=>{
+    const id = req.params.id;
+    const product = repoContext.products.findProductById(id);
+    return res.send(product)
 })
